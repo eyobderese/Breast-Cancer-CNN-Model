fd04271759757c0b6f509dfec73bf208dd099be5
eyobderese <eyob.derese@aait.edu.et>
2024-11-13 09:42:45 +0300

    saving model

--- train_model.py
+++ train_model.py
@@ -18,7 +18,7 @@ matplotlib.use("Agg")
 plot_path = "plot.png"
 
 # Set training parameters
-NUM_EPOCHS = 30
+NUM_EPOCHS = 40
 INIT_LR = 1e-2
 BS = 32
 
@@ -102,39 +102,39 @@ H = model.fit(
     validation_steps=totalVal // BS,
     epochs=NUM_EPOCHS)
 
-# # Evaluate the model
-# print("[INFO] evaluating network...")
-# testGen.reset()
-# predIdxs = model.predict(x=testGen, steps=(totalTest // BS) + 1)
-# predIdxs = np.argmax(predIdxs, axis=1)
-# print(classification_report(testGen.classes, predIdxs,
-#       target_names=testGen.class_indices.keys()))
+# Evaluate the model
+print("[INFO] evaluating network...")
+testGen.reset()
+predIdxs = model.predict(x=testGen, steps=(totalTest // BS) + 1)
+predIdxs = np.argmax(predIdxs, axis=1)
+print(classification_report(testGen.classes, predIdxs,
+      target_names=testGen.class_indices.keys()))
 
-# # Calculate confusion matrix, accuracy, sensitivity, and specificity
-# cm = confusion_matrix(testGen.classes, predIdxs)
-# total = sum(sum(cm))
-# acc = (cm[0, 0] + cm[1, 1]) / total
-# sensitivity = cm[0, 0] / (cm[0, 0] + cm[0, 1])
-# specificity = cm[1, 1] / (cm[1, 0] + cm[1, 1])
-# print(cm)
-# print("acc: {:.4f}".format(acc))
-# print("sensitivity: {:.4f}".format(sensitivity))
-# print("specificity: {:.4f}".format(specificity))
+# Calculate confusion matrix, accuracy, sensitivity, and specificity
+cm = confusion_matrix(testGen.classes, predIdxs)
+total = sum(sum(cm))
+acc = (cm[0, 0] + cm[1, 1]) / total
+sensitivity = cm[0, 0] / (cm[0, 0] + cm[0, 1])
+specificity = cm[1, 1] / (cm[1, 0] + cm[1, 1])
+print(cm)
+print("acc: {:.4f}".format(acc))
+print("sensitivity: {:.4f}".format(sensitivity))
+print("specificity: {:.4f}".format(specificity))
 
-# # Save the model
-# model.save("cancer_detection_model")
-# print("[INFO] Model saved to disk.")
+# Save the model
+model.save("cancer_detection_model.h5")
+print("[INFO] Model saved to disk.")
 
-# # Plot the training loss and accuracy
-# N = NUM_EPOCHS
-# plt.style.use("ggplot")
-# plt.figure()
-# plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-# plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-# plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
-# plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
-# plt.title("Training Loss and Accuracy on Dataset")
-# plt.xlabel("Epoch #")
-# plt.ylabel("Loss/Accuracy")
-# plt.legend(loc="lower left")
-# plt.savefig(plot_path)
+# Plot the training loss and accuracy
+N = NUM_EPOCHS
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy on Dataset")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig(plot_path)
